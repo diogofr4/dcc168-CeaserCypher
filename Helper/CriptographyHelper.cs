28af668ad2015a71df8851208c117536b998dd05
diogofr4 <diogofr4@gmail.comm>
2022-10-11 22:12:04 -0300

    Add project files.

---
+++ Helper/CriptographyHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeaserCypher.Helper
+{
+    public static class CriptographyHelper
+    {
+        /// <summary>
+        /// Método que chama a encriptação para cada letra.
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
+        public static string Encrypt(string plainText)
+        {
+            var encrypted = string.Empty;
+            foreach(char c in plainText)
+            {
+                encrypted += EncryptAlgorithm(c);
+            }
+
+            return encrypted;
+        }
+
+        /// <summary>
+        /// Método que chama a decriptação para cada letra.
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
+        public static string Decrypt(string encryptedText)
+        {
+            var decrypted = string.Empty;
+            foreach (char c in encryptedText)
+            {
+                decrypted += DecryptAlgorithm(c);
+            }
+
+            return decrypted;
+        }
+
+        /// <summary>
+        /// Método que aplica a encriptação de César em um letra utilizando índice 3.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private static char EncryptAlgorithm(char c)
+        {
+            if (!char.IsLetter(c))
+            {
+                return c;
+            }
+
+            char d = char.IsUpper(c) ? 'A' : 'a';
+            return (char)((((c + 3) - d) % 26) + d);
+        }
+
+        /// <summary>
+        /// Método que aplica a decriptação de César em um letra utilizando índice 3.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private static char DecryptAlgorithm(char c)
+        {
+            if (!char.IsLetter(c))
+            {
+                return c;
+            }
+
+            char d = char.IsUpper(c) ? 'A' : 'a';
+            return (char)((((c + 23) - d) % 26) + d);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CeaserCypher.Helper;
+
+Console.WriteLine("Texto a ser encriptado:");
+string plainText = Console.ReadLine();
+
+Console.WriteLine("\n");
+
+var encrypted = CriptographyHelper.Encrypt(plainText);
+var decrypted = CriptographyHelper.Decrypt(encrypted);
+
+Console.WriteLine("Texto encriptado:");
+Console.WriteLine(encrypted);
+Console.WriteLine("\n");
+Console.WriteLine("Texto decriptado:");
+Console.WriteLine(decrypted);
